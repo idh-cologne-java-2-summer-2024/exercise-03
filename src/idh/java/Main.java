789d67a053fa0d5f7864fece65a86c5fbe586a1d
jessica <jessica@jessicas-mbp>
2023-04-24 12:30:49 +0200

    kopfhörer

--- src/idh/java/Main.java
+++ src/idh/java/Main.java
@@ -6,8 +6,8 @@ public class Main {
 		
 		// Inspiriert von: https://de.wikipedia.org/wiki/Kofferpacken
 		// Wer hätte gedacht dass es dazu einen Wikipedia-Artikel gibt?
-		System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon.");
+		System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon und meine Kopfhörer.");
 		
-		System.out.println("Ich packe in meinen Koffer meine Kopfhörer.");
+		
 	};
 }
