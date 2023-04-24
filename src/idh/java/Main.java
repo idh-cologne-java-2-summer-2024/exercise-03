61853a553805bac8ee255c30a8c52f32df381a67
EricXKl <eric.c.kloeckner@gmail.com>
2023-04-24 15:18:26 +0200

    Merged

--- src/idh/java/Main.java
+++ src/idh/java/Main.java
@@ -7,5 +7,9 @@ public class Main {
 		// Wer hätte gedacht dass es dazu einen Wikipedia-Artikel gibt?		
 		System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon und ein Gummihuhn und ein Apache attack helicopter und ein Faultier und ein Kartenspiel und eine Erstausgabe des First Folio mit einem Knick in der linken oberen Ecke der 313. Seite und eine Sonnenbrille und ein Handy und eine Kartoffelsuppe und ein Eis und ein Handtuch und ein Stabmixer und Prof. Dr. Øyvind Eide und einen Bassisten einer Baby-Katzen-Metalband und einen Beutel voller Würfel und mein Tagebuch und meine Kopfhörer und ein Kochbuch und ein IPad und eine Schiffsmodell und und einen Fernseher für Unterhaltung und eine Flasche Gilden und Donaudampfschiffahrtsgesellschaftskapitän.");
 
+
+
+
+        //'main' of https://github.com/idh-cologne-java-2-summer-2023/exercise-03
 	};
 }
