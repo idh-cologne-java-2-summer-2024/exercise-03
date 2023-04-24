b0522bbee971958a97e40d987ac6bd8750407ced
NicoS0910 <Nico.schmidt2001@gmx.net>
2023-04-24 14:08:11 +0200

    Added IPad

--- src/idh/java/Main.java
+++ src/idh/java/Main.java
@@ -4,10 +4,10 @@ public class Main {
 	public static void main(String[] args) {
 
 		// Inspiriert von: https://de.wikipedia.org/wiki/Kofferpacken
-		// Wer hätte gedacht dass es dazu einen Wikipedia-Artikel gibt?
+		// Wer hï¿½tte gedacht dass es dazu einen Wikipedia-Artikel gibt?
 
 		
-		System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon und ein Gummihuhn und ein Apache attack helicopter und ein Faultier und ein Kartenspiel und eine Erstausgabe des First Folio mit einem Knick in der linken oberen Ecke der 313. Seite und eine Sonnenbrille und ein Handy und eine Kartoffelsuppe und ein Eis und ein Handtuch und ein Stabmixer und Prof. Dr. Ã˜yvind Eide und einen Bassisten einer Baby-Katzen-Metalband und einen Beutel voller Würfel und mein Tagebuch und meine Kopfhörer und ein Kochbuch.");
+		System.out.println("Ich packe in meinen Koffer ein Netzteil fï¿½r mein Telefon und ein Gummihuhn und ein Apache attack helicopter und ein Faultier und ein Kartenspiel und eine Erstausgabe des First Folio mit einem Knick in der linken oberen Ecke der 313. Seite und eine Sonnenbrille und ein Handy und eine Kartoffelsuppe und ein Eis und ein Handtuch und ein Stabmixer und Prof. Dr. Ã˜yvind Eide und einen Bassisten einer Baby-Katzen-Metalband und einen Beutel voller Wï¿½rfel und mein Tagebuch und meine Kopfhï¿½rer und ein Kochbuch und ein IPad.");
 
 
 	};
