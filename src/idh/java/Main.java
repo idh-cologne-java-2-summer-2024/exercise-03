983c2e0894ee27d7b3748ab53b99b77ad14e089a
TitusOsberghaus <titus.osberghau@gmail.com>
2023-04-25 18:47:44 +0200

    Crocks

--- src/idh/java/Main.java
+++ src/idh/java/Main.java
@@ -6,6 +6,6 @@ public class Main {
 		
 		// Inspiriert von: https://de.wikipedia.org/wiki/Kofferpacken
 		// Wer hätte gedacht dass es dazu einen Wikipedia-Artikel gibt?
-		System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon und ein Telefon.");
+		System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon und ein Telefon." + "und meine Crocks");
 	};
 }
