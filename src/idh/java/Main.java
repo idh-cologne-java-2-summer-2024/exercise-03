f297d24d122c1bb8e5e9cc233c91d045e219a9a2
javi1337 <jan@pulsarstudio.de>
2023-04-25 21:04:16 +0200

    Kamera

--- src/idh/java/Main.java
+++ src/idh/java/Main.java
@@ -6,6 +6,6 @@ public class Main {
 		
 		// Inspiriert von: https://de.wikipedia.org/wiki/Kofferpacken
 		// Wer hätte gedacht dass es dazu einen Wikipedia-Artikel gibt?
-         System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon und eine Wasserflasche und einen Hamster und ein Telefon und meine Crocks und eine Wasserflasche und meine Zahnbürste und ein gutes Java-Buch und einen Pfannekuchen und noch eine Zahnbürste und Gaffel Wiess.");
+         System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon und eine Wasserflasche und einen Hamster und ein Telefon und meine Crocks und eine Wasserflasche und meine Zahnbürste und ein gutes Java-Buch und einen Pfannekuchen und noch eine Zahnbürste und Gaffel Wiess und eine Kamera.");
 	};
 }
