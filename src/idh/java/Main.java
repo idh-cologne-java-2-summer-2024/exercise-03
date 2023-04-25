e15461d5d7e9624b793b6a6d16269e9d74bd81f6
beyzat <beyzaturel@localhost>
2023-04-25 20:49:10 +0200

    Bratpfanne

--- src/idh/java/Main.java
+++ src/idh/java/Main.java
@@ -6,7 +6,7 @@ public class Main {
 		
 		// Inspiriert von: https://de.wikipedia.org/wiki/Kofferpacken
 		// Wer hätte gedacht dass es dazu einen Wikipedia-Artikel gibt?
-         System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon und eine Wasserflasche und einen Hamster und ein Telefon und meine Crocks und eine Wasserflasche und meine Zahnbürste und ein gutes Java-Buch und einen Pfannekuchen.");
+         System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon und eine Wasserflasche und einen Hamster und ein Telefon und meine Crocks und eine Wasserflasche und meine Zahnbürste und ein gutes Java-Buch und einen Pfannekuchen und eine Bratpfanne.");
 		//* Pfannekuchen hinzugefügt
 	    
 	};
