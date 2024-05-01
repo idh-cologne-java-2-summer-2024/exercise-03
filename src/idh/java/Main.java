00abeb0750daecc8ee485872ec86381e2b54bcb8
Yuima <Yuima@DwieDosenlaptop>
2024-05-01 20:38:37 +0200

    Datei gemerget und mein Element dem Koffer hinzugefügt

--- src/idh/java/Main.java
+++ src/idh/java/Main.java
@@ -6,11 +6,7 @@ public class Main {
 	// Inspiriert von: https://de.wikipedia.org/wiki/Kofferpacken
 	// Wer hätte gedacht dass es dazu einen Wikipedia-Artikel gibt?
 
-<<<<<<< HEAD
-	System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon. Ich packe eine Tube Zahnbelag dazu.");
-=======
-	System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon, eine Gartenschere und einen doppelten Boden.");
->>>>>>> branch 'main' of https://github.com/idh-cologne-java-2-summer-2024/exercise-03.git
+	System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon, eine Gartenschere, einen doppelten Boden und eine Tube Zahnbelag.");
     }
 
 }
