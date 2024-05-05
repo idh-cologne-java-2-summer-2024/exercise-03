f03cee11d3ffbef3736fdfdced4d56823bfb3a2e
Fana <Fana@DESKTOP-ARPHR48.fritz.box>
2024-05-05 15:15:47 +0200

    Resolved the conflict. Hopefully.

--- src/idh/java/Main.java
+++ src/idh/java/Main.java
@@ -6,12 +6,9 @@ public class Main {
 	// Inspiriert von: https://de.wikipedia.org/wiki/Kofferpacken
 	// Wer hätte gedacht dass es dazu einen Wikipedia-Artikel gibt?
 
-<<<<<<< HEAD
-	System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon und eine neue Zahnbuerste.");
-=======
-	System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon, meinen Laptop, und eine Socke");
 
->>>>>>> branch 'main' of https://github.com/idh-cologne-java-2-summer-2024/exercise-03.git
+	System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon, meinen Laptop, und eine Socke und eine neue Zahnbuerste.");
+
     }
 
 }
