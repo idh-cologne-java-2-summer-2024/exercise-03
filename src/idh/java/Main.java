8a23c046c8b4c3c1b5435465df9b3b0f7a8b5d51
marvi <marvi@DESKTOP-37RQBMC.fritz.box>
2024-05-06 19:17:36 +0200

    somehow not shown on github as committed

--- src/idh/java/Main.java
+++ src/idh/java/Main.java
@@ -6,12 +6,7 @@ public class Main {
 	// Inspiriert von: https://de.wikipedia.org/wiki/Kofferpacken
 	// Wer hätte gedacht dass es dazu einen Wikipedia-Artikel gibt?
 
-<<<<<<< HEAD
-	System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon und ein Bügeleisen");
-    }
-=======
-System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon, meinen Laptop, und eine Socke und eine neue Zahnbuerste, dazu eine Sitar, den zweiten Socken.");
->>>>>>> branch 'main' of https://github.com/idh-cologne-java-2-summer-2024/exercise-03.git
+	System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon, meinen Laptop, und eine Socke und eine neue Zahnbuerste, dazu eine Sitar, den zweiten Socken und ein Bügeleisen");
 
 
     }
