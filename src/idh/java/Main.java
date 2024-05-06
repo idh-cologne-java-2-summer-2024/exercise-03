a918685832d16460727f06e1acb780348d4dd372
edylyn <edylyn@edys-mbp.fritz.box>
2024-05-07 01:46:56 +0200

    commit Part 2

--- src/idh/java/Main.java
+++ src/idh/java/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	// Inspiriert von: https://de.wikipedia.org/wiki/Kofferpacken
 	// Wer hätte gedacht dass es dazu einen Wikipedia-Artikel gibt?
 
-	System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon, meinen Laptop, und eine Socke und eine neue Zahnbuerste, dazu eine Sitar, den zweiten Socken und ein Bügeleisen und meine Kopfhörer und 3L Coldbrew.");
+	System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon, meinen Laptop, und eine Socke und eine neue Zahnbuerste, dazu eine Sitar, den zweiten Socken und ein Bügeleisen und meine Kopfhörer und 3 Cups of Coldbrew.");
 
 
 
