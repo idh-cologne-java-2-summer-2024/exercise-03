12e02bf8e20bac2944fdaaf3bbefe2caf03c6a01
mulut <mulut@LAPTOP-89CGKF1L.speedport.ip>
2024-05-06 22:49:39 +0200

    merge version

--- src/idh/java/Main.java
+++ src/idh/java/Main.java
@@ -6,12 +6,8 @@ public class Main {
 	// Inspiriert von: https://de.wikipedia.org/wiki/Kofferpacken
 	// Wer hätte gedacht dass es dazu einen Wikipedia-Artikel gibt?
 
-<<<<<<< HEAD
-	System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon und ein Schwert.");
-    }
-=======
-	System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon, meinen Laptop, und eine Socke und eine neue Zahnbuerste, dazu eine Sitar, den zweiten Socken und ein Bügeleisen");
->>>>>>> branch 'main' of https://github.com/idh-cologne-java-2-summer-2024/exercise-03.git
+	System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon, meinen Laptop, und eine Socke und eine neue Zahnbuerste, dazu eine Sitar, den zweiten Socken und ein Bügeleisen und meine Kopfhörer.");
+
 
 
     }
