c2946a03c7c51e8d28c3cd39c979bbe2691fcef6
leoni <leoni@Leonies_Laptop.fritz.box>
2024-05-07 12:08:24 +0200

    2 versuch

--- src/idh/java/Main.java
+++ src/idh/java/Main.java
@@ -5,13 +5,7 @@ public class Main {
 	// Inspiriert von: https://de.wikipedia.org/wiki/Kofferpacken
 	// Wer hätte gedacht dass es dazu einen Wikipedia-Artikel gibt?
 
-<<<<<<< HEAD
-	System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon und ein Baummkuchen.");
-    }
 
-}
-=======
-
-	System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon, meinen Laptop, und eine Socke und eine neue Zahnbuerste, dazu eine Sitar, den zweiten Socken und ein Bügeleisen meine Kopfhörer, ein Schachbrett und meine Hunde sowie ein Schwimmtier.");}}
+	System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon, meinen Laptop, und eine Socke und eine neue Zahnbuerste, dazu eine Sitar, den zweiten Socken und ein Bügeleisen meine Kopfhörer, ein Schachbrett und meine Hunde sowie ein Schwimmtier und ein Baummkuchen.");}}
 	
->>>>>>> branch 'main' of https://github.com/idh-cologne-java-2-summer-2024/exercise-03
+
