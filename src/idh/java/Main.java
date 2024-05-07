9460cbbc066aafa0e1dedc2db332fa3d4f3d4505
mehmetalisahin <mehmetalisahin@macbook-air-mali.fritz.box>
2024-05-07 21:39:46 +0200

    huh

--- src/idh/java/Main.java
+++ src/idh/java/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	// Inspiriert von: https://de.wikipedia.org/wiki/Kofferpacken
 	// Wer hätte gedacht dass es dazu einen Wikipedia-Artikel gibt?
 
-	System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon, eine Gartenschere, einen doppelten Boden, meinen Laptop, und eine Socke und eine neue Zahnbuerste, dazu eine Sitar, den zweiten Socken und ein Bügeleisen meine Kopfhörer, einen Chociatto, ein Schachbrett und meine Hunde sowie ein Schwimmtier und ein Baummkuchen. Eine große Tube Zahnbelag darf allerdings auch nicht fehlen. Außerdem eine Ukuklele mit Stimmgerät und zu guter letzt eine Badenudel damit ich nicht untergehe und Koffein und ein Donut.Ein Steam Deck darf natürlich auch nicht fehlen.\"");
+	System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon, eine Gartenschere, einen doppelten Boden, meinen Laptop, und eine Socke und eine neue Zahnbuerste, dazu eine Sitar, den zweiten Socken und ein Bügeleisen meine Kopfhörer, einen Chociatto, ein Schachbrett und meine Hunde sowie ein Schwimmtier und ein Baummkuchen. Eine große Tube Zahnbelag darf allerdings auch nicht fehlen. Außerdem eine Ukuklele mit Stimmgerät und zu guter letzt eine Badenudel damit ich nicht untergehe und Koffein und ein Donut.Ein Steam Deck darf auch nicht fehlen.\"");
     }
 
 
