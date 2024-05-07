fc57cdb8a36c4b6744be727c6f849934b48170f3
locko <locko@172.29.195.110>
2024-05-07 13:55:21 +0200

    ...

--- src/idh/java/Main.java
+++ src/idh/java/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
 	// Inspiriert von: https://de.wikipedia.org/wiki/Kofferpacken
-	// Wer hätte gedacht dass es dazu einen Wikipedia-Artikel gibt?
+	// Wer hätte gedacht dass es dazu einen Wikipedia-Artikel gibt??
 
 	System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon, einen Chociatto");
     }
