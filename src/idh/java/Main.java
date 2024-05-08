5d96fd5f5d56549ce527bbc75f9b4fbecc4c5b7e
Yuima <Yuima@172.29.208.176>
2024-05-08 12:13:08 +0200

    Pferd aus dem Graben der Vergessenheit gerettet.

--- src/idh/java/Main.java
+++ src/idh/java/Main.java
@@ -2,17 +2,10 @@ package idh.java;
 
 public class Main {
     public static void main(String[] args) {
-	// Inspiriert von: https://de.wikipedia.org/wiki/Kofferpacken
-	// Wer hätte gedacht dass es dazu einen Wikipedia-Artikel gibt??
+	// 	Inspiriert von: https://de.wikipedia.org/wiki/Kofferpacken
+	// 	Wer hätte gedacht dass es dazu einen Wikipedia-Artikel gibt??
 
-<<<<<<< HEAD
-	System.out.println("Ich packe in meinen Koffer ein Pferd mit Sattel.");
-    }
-=======
->>>>>>> branch 'main' of https://github.com/idh-cologne-java-2-summer-2024/exercise-03
-
-
-    	System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon, eine Gartenschere, einen doppelten Boden, meinen Laptop, und eine Socke und eine neue Zahnbuerste, dazu eine Sitar, den zweiten Socken und ein Bügeleisen meine Kopfhörer, einen Chociatto, ein Schachbrett und meine Hunde sowie ein Schwimmtier und ein Baummkuchen. Eine große Tube Zahnbelag darf allerdings auch nicht fehlen.");
+    	System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon, eine Gartenschere, einen doppelten Boden, meinen Laptop, und eine Socke und eine neue Zahnbuerste, dazu eine Sitar, den zweiten Socken und ein Bügeleisen meine Kopfhörer, einen Chociatto, ein Schachbrett und meine Hunde sowie ein Schwimmtier, ein Baummkuchen und ein Pferd mit Sattel. Eine große Tube Zahnbelag darf allerdings auch nicht fehlen.");
 	}
 }
 	
