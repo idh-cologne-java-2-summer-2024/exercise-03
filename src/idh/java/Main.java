13a0fe6ade0a9e4f59be36e4e12b1f7c0c45ee01
Anwender <Anwender@MomoDesktop.fritz.box>
2024-05-14 16:09:52 +0200

    hund

--- src/idh/java/Main.java
+++ src/idh/java/Main.java
@@ -2,7 +2,7 @@ package idh.java;
 
 public class Main {
     public static void main(String[] args) {
-    	System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon, eine Gartenschere, einen doppelten Boden, meinen Laptop, und eine Socke und eine neue Zahnbuerste, dazu eine Sitar, den zweiten Socken und ein Bügeleisen meine Kopfhörer, einen Chociatto, ein Schachbrett und meine Hunde sowie ein Schwimmtier, ein Baummkuchen, ein Kuscheltier und ein Pferd mit Sattel. Eine große Tube Zahnbelag darf allerdings auch nicht fehlen.");
+    	System.out.println("Ich packe in meinen Koffer ein Netzteil für mein Telefon, eine Gartenschere, einen doppelten Boden, meinen Laptop, und eine Socke und eine neue Zahnbuerste, dazu eine Sitar, den zweiten Socken und ein Bügeleisen meine Kopfhörer, einen Chociatto, ein Schachbrett und meine Hunde sowie ein Schwimmtier, ein Baummkuchen, ein Kuscheltier und ein Pferd mit Sattel. Eine große Tube Zahnbelag darf allerdings auch nicht fehlen und natürlich meinen Hund :).");
 	}
 }
 	
